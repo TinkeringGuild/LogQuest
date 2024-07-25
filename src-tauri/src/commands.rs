82a4247d1415d14e5b76c6601e316207015c29bd
Xenk <XenkP99@users.noreply.github.com>
2024-07-25 06:55:36 -0500

    reformatting files

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -5,85 +5,85 @@ use std::path::PathBuf;
 use tauri::{AppHandle, Manager};
 
 pub fn handler() -> impl Fn(tauri::Invoke) {
-    tauri::generate_handler![
-        print_to_stdout,
-        print_to_stderr,
-        get_config,
-        set_everquest_dir,
-        import_gina_triggers_file
-    ]
+  tauri::generate_handler![
+    print_to_stdout,
+    print_to_stderr,
+    get_config,
+    set_everquest_dir,
+    import_gina_triggers_file
+  ]
 }
 
 #[tauri::command]
 fn get_config(app_handle: AppHandle) -> Result<LogQuestConfig, String> {
-    let app_state = app_handle.state::<AppState>();
-    let config = app_state.config.lock().unwrap().clone();
-    Ok(config)
+  let app_state = app_handle.state::<AppState>();
+  let config = app_state.config.lock().unwrap().clone();
+  Ok(config)
 }
 
 #[tauri::command]
 fn import_gina_triggers_file(
-    app_handle: AppHandle,
-    _path: String,
+  app_handle: AppHandle,
+  _path: String,
 ) -> Result<LogQuestConfig, String> {
-    let app_state = app_handle.state::<AppState>();
-    let config = app_state.config.lock().unwrap().clone();
-    Ok(config)
+  let app_state = app_handle.state::<AppState>();
+  let config = app_state.config.lock().unwrap().clone();
+  Ok(config)
 }
 
 #[tauri::command]
 fn set_everquest_dir(app_handle: AppHandle, new_dir: String) -> Result<LogQuestConfig, String> {
-    let Ok(new_dir) = canonicalize(new_dir) else {
-        return Err(String::from("Could not determine canonical path of EQ dir"));
-    };
-    let new_dir = new_dir.to_str().unwrap();
-    validate_eq_dir(&PathBuf::from(new_dir))?;
-    with_config(&app_handle, |config| {
-        config.everquest_directory = Some(new_dir.to_string());
-        Ok(())
-    })
+  let Ok(new_dir) = canonicalize(new_dir) else {
+    return Err(String::from("Could not determine canonical path of EQ dir"));
+  };
+  let new_dir = new_dir.to_str().unwrap();
+  validate_eq_dir(&PathBuf::from(new_dir))?;
+  with_config(&app_handle, |config| {
+    config.everquest_directory = Some(new_dir.to_string());
+    Ok(())
+  })
 }
 
 #[tauri::command]
 fn print_to_stdout(message: String) {
-    println!("[UI] {}", message);
+  println!("[UI] {}", message);
 }
 
 #[tauri::command]
 fn print_to_stderr(message: String) {
-    eprintln!("[UI ERROR] {}", message);
+  eprintln!("[UI ERROR] {}", message);
 }
 
 /// Yields a mutable borrow to the LogQuestConfig and automatically saves the file if any changes are made.
 fn with_config<F>(app_handle: &AppHandle, f: F) -> Result<LogQuestConfig, String>
 where
-    F: FnOnce(&mut LogQuestConfig) -> Result<(), String>,
+  F: FnOnce(&mut LogQuestConfig) -> Result<(), String>,
 {
-    let app_state = app_handle.state::<AppState>();
-    let mut config_guard = app_state
-        .config
-        .lock()
-        .expect("Could not obtain lock for the LogQuestConfig");
-    let config_before: LogQuestConfig = config_guard.clone();
-    f(&mut *config_guard)?;
-    if *config_guard != config_before {
-        println!(
-            "SAVING CONFIG TO {}",
-            config_before.config_file_path.to_string_lossy()
-        );
-        config_guard.save().map_err(|e| e.to_string())?;
-    }
-    Ok(config_guard.clone())
+  let app_state = app_handle.state::<AppState>();
+  let mut config_guard = app_state
+    .config
+    .lock()
+    .expect("Could not obtain lock for the LogQuestConfig");
+  let config_before: LogQuestConfig = config_guard.clone();
+  f(&mut *config_guard)?;
+  if *config_guard != config_before {
+    println!(
+      "SAVING CONFIG TO {}",
+      config_before.config_file_path.to_string_lossy()
+    );
+    config_guard.save().map_err(|e| e.to_string())?;
+  }
+  Ok(config_guard.clone())
 }
 
 fn validate_eq_dir(path: &PathBuf) -> Result<(), String> {
-    let eqclient_file = path.join("eqclient.ini");
-    if eqclient_file.exists() {
-        Ok(())
-    } else {
-        Err(format!(
-            "The path '{}' is not a valid EverQuest directory!",
-            path.to_str().unwrap()
-        ))
-    }
+  let eqclient_file = path.join("eqclient.ini");
+  if eqclient_file.exists() {
+    Ok(())
+  } else {
+    Err(format!(
+      "The path '{}' is not a valid EverQuest directory!",
+      path.to_str().unwrap()
+    ))
+  }
 }
--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -10,74 +10,74 @@ const CONFIG_FILE_NAME: &str = "LogQuest.toml";
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, TS)]
 pub struct LogQuestConfig {
-    pub everquest_directory: Option<String>,
+  pub everquest_directory: Option<String>,
 
-    #[serde(skip)]
-    #[ts(skip)]
-    pub config_file_path: PathBuf,
+  #[serde(skip)]
+  #[ts(skip)]
+  pub config_file_path: PathBuf,
 }
 
 impl LogQuestConfig {
-    fn new_with_path(config_file_path: &PathBuf) -> Self {
-        LogQuestConfig {
-            everquest_directory: None,
-            config_file_path: config_file_path.to_owned(),
-        }
+  fn new_with_path(config_file_path: &PathBuf) -> Self {
+    LogQuestConfig {
+      everquest_directory: None,
+      config_file_path: config_file_path.to_owned(),
     }
+  }
 
-    fn load_from_file_path(path: &PathBuf) -> anyhow::Result<Self> {
-        let raw_config_file = fs::read_to_string(path)?;
-        let mut config: LogQuestConfig = toml::from_str(&raw_config_file)?;
-        config.config_file_path = path.to_owned();
-        Ok(config)
+  fn load_from_file_path(path: &PathBuf) -> anyhow::Result<Self> {
+    let raw_config_file = fs::read_to_string(path)?;
+    let mut config: LogQuestConfig = toml::from_str(&raw_config_file)?;
+    config.config_file_path = path.to_owned();
+    Ok(config)
+  }
+
+  pub fn save(&self) -> anyhow::Result<()> {
+    self.write_to_file_path(&self.config_file_path)
+  }
+
+  fn write_to_file_path(&self, path: &PathBuf) -> anyhow::Result<()> {
+    let raw_toml = toml::to_string_pretty(&self)?;
+
+    if let Some(parent) = path.parent() {
+      if !parent.exists() {
+        println!("Creating directory {}", parent.display());
+        fs::create_dir_all(&parent)?;
+      }
     }
-
-    pub fn save(&self) -> anyhow::Result<()> {
-        self.write_to_file_path(&self.config_file_path)
-    }
-
-    fn write_to_file_path(&self, path: &PathBuf) -> anyhow::Result<()> {
-        let raw_toml = toml::to_string_pretty(&self)?;
-
-        if let Some(parent) = path.parent() {
-            if !parent.exists() {
-                println!("Creating directory {}", parent.display());
-                fs::create_dir_all(&parent)?;
-            }
-        }
-        let mut file = File::create(path)?; // overwrites file if it already exists
-        file.write_all(raw_toml.as_bytes())?;
-        Ok(())
-    }
+    let mut file = File::create(path)?; // overwrites file if it already exists
+    file.write_all(raw_toml.as_bytes())?;
+    Ok(())
+  }
 }
 
 pub fn load_app_config(config_dir: &PathBuf) -> anyhow::Result<LogQuestConfig> {
-    let config_path = config_dir.join(&CONFIG_FILE_NAME);
+  let config_path = config_dir.join(&CONFIG_FILE_NAME);
 
-    let config = if config_path.exists() {
-        LogQuestConfig::load_from_file_path(&config_path)?
-    } else {
-        let config = LogQuestConfig::new_with_path(&config_path);
-        config.save()?;
-        config
-    };
-    Ok(config)
+  let config = if config_path.exists() {
+    LogQuestConfig::load_from_file_path(&config_path)?
+  } else {
+    let config = LogQuestConfig::new_with_path(&config_path);
+    config.save()?;
+    config
+  };
+  Ok(config)
 }
 
 pub fn get_config_dir_with_optional_override(
-    path_override: Option<&PathBuf>,
+  path_override: Option<&PathBuf>,
 ) -> anyhow::Result<PathBuf> {
-    let config_dir = match path_override {
-        Some(overridden_dir) => overridden_dir.to_owned(),
-        None => match config_dir() {
-            Some(mut dir) => {
-                let app_name = env!("CARGO_PKG_NAME");
-                dir.push(app_name);
-                dir
-            }
-            None => bail!("Could not determine the config directory"),
-        },
-    };
-    fs::create_dir_all(&config_dir)?;
-    Ok(config_dir)
+  let config_dir = match path_override {
+    Some(overridden_dir) => overridden_dir.to_owned(),
+    None => match config_dir() {
+      Some(mut dir) => {
+        let app_name = env!("CARGO_PKG_NAME");
+        dir.push(app_name);
+        dir
+      }
+      None => bail!("Could not determine the config directory"),
+    },
+  };
+  fs::create_dir_all(&config_dir)?;
+  Ok(config_dir)
 }
